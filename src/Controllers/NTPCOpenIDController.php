1a90c957232e255d7ee428edc2d2ad43c1a306d9
林士立 <t301000@gmail.com>
2018-09-16 13:31:15 +0800

    改用 app() 取代建構式取得 NtpcOpenid 實體
    避免 php artisan route:list 時產生錯誤

--- src/Controllers/NTPCOpenIDController.php
+++ src/Controllers/NTPCOpenIDController.php
@@ -8,42 +8,37 @@ use T301000\LaravelNtpcOpenid\NtpcOpenid;
 
 class NTPCOpenIDController extends Controller
 {
-    protected $openid = null;
-
-    public function __construct(NtpcOpenid $openid)
-    {
-        $this->openid = $openid;
-    }
-
     /**
      * 啟動 OpenID 認證流程
      */
     public function startOpenID()
     {
-        return redirect($this->openid->authUrl());
+        $openid = app('ntpcopenid');
+        return redirect($openid->authUrl());
     }
 
     /**
      * OpenID 導回後之處理
      */
-    public function process() 
+    public function process()
     {
-        switch ($this->openid->mode) {
+        $openid = app('ntpcopenid');
+        switch ($openid->mode) {
             case 'cancel': // 取消授權
                 return redirect(config('ntpcopenid.redirectToUrls.user_cancel'));
                 break;
 
             case 'id_res': // 同意授權
-                if (!$this->openid->validate()) {
+                if (!$openid->validate()) {
                     // 驗證未過              
                     return redirect(config('ntpcopenid.redirectToUrls.validate_fail'));
                 }
                 
                 // 驗證通過，檢查是否允許登入
-                if($this->openid->canLogin()) {
+                if($openid->canLogin()) {
                     // 允許登入
                     // 取得 user data 陣列
-                    $data = $this->openid->getUserData('*');
+                    $data = $openid->getUserData('*');
                     
                     // 將取得的資料存入 session
                     session([config('ntpcopenid.sessionKey') => $data]);
